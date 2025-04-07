bde5ea48429ab67f4c7f3d7e4a25fac25556039b
Michiel Oda <michiel.oda@skyline.be>
2025-04-07 14:12:54 +0200

    Add project files.

---
+++ MyScript_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyScript_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("MyScript_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("63BD1D86-F68A-4B09-862D-8AB6E27298F7")]
+[assembly: AssemblyVersion("1.0.0.0")]
